263916230a54fe71e9299db7107c2ef1af11bfe4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 10:44:51 +0530

    Add files via upload

---
+++ TaskManagementApp/DAL/Services/TaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TaskManagementApp.DAL.Interfaces;
+
+namespace TaskManagementApp.DAL.Services
+{
+    public class TaskRepository : Interfaces.ITaskRepository
+    {
+        private TaskDbContext _context;
+
+        public TaskRepository(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.TaskModel GetById(int id)
+        {
+            return _context.TaskModels.FirstOrDefault(t => t.Id == id);
+        }
+
+        public string GetAll()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskManagementApp/DAL/Services/TaskService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TaskManagementApp.DAL.Interfaces;
+using TaskManagementApp.Model;
+
+namespace TaskManagementApp.DAL.Services
+{
+    public class TaskService : Interfaces.ITaskService
+    {
+        private Interfaces.ITaskRepository _repository;
+
+        public TaskService(Interfaces.ITaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            //Write your code here
+            throw new NotImplementedException(); 
+        }
+
+        public string Add()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //Write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
